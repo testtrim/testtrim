54bcdc81425d5a3cd760c7a1c8f20dc0b8ed5166
Mathieu Fenniak <mathieu@fenniak.net>
2024-08-28 19:37:39 +0200

    feat: commit-based analysis tool

---
+++ scripts/rust-git-analysis.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+# set -x
+
+
+COMMIT=$1
+
+echo "Preparing data for commit $COMMIT..."
+
+# I need two pieces of data:
+# - the "test coverage map" from the commit before $COMMIT, which is what rust-test-runner.sh produces
+# - the files modified in $COMMIT (in the future, maybe the functions too)
+#
+# This would accurately represent the situation where we know the coverage data from the last commit, but want to figure
+# out what to run for tests in this commit.
+
+# For the "test coverage map"; checkout the commit from before $COMMIT and generate it.
+
+git archive --format=tgz --output=archive-pre-$COMMIT.tgz $COMMIT^
+
+rm -rf tmp-$COMMIT
+mkdir -p tmp-$COMMIT
+pushd tmp-$COMMIT
+
+tar -xvf ../archive-pre-$COMMIT.tgz
+rm ../archive-pre-$COMMIT.tgz
+../rust-test-runner.sh
+
+# Remove unnecessary files... probably ought to clean this up in rust-test-runner.sh
+find coverage-output | grep profraw$ | xargs rm
+find coverage-output | grep profdata$ | xargs rm
+
+tar -jcvf ../coverage-output-pre-$COMMIT.tar.bz2 coverage-output
+
+# That concludes the "test coverage map"; now I just need to know what files were affected.  That's easy:
+git diff --name-only $COMMIT^ $COMMIT > ../coverage-files-changed-$COMMIT.txt
+
+popd
+rm -rf tmp-$COMMIT
