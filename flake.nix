35855a3301288c7273133ed03790aab8108ff97d
Mathieu Fenniak <mathieu@fenniak.net>
2026-08-31 04:45:18 +0200

    chore: add build artifact cache (#1129)
    Reviewed-on: https://codeberg.org/testtrim/testtrim/pulls/1129

--- flake.nix
+++ flake.nix
@@ -7,7 +7,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
   };
 
